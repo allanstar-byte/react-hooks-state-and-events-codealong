12157f865c3b9f622de712eefb5af33413ec0f7b
allanstar-byte <allanmutisya263@gmail.com>
2022-07-18 12:53:56 +0300

    state

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,47 @@
 import React from "react";
+import react, { useState } from "react";
+
+// function Toggle() {
+//     const [isOn, setIsOn] = useState(false);
+
+//     function handleClick() {
+//         setIsOn((isOn) => !isOn);
+//     }
+
+//     return <button onClick = { handleClick } > { isOn ? "ON" : "OFF" } < /button>;
+
+//     // return <button onClick = { handleClick } > {
+//     //     if (isOn) {
+//     //         return ("ON")
+//     //     } else {
+//     //         "OFF"
+//     //     }
+//     // } < /button>;
+// }
+
 
 function Toggle() {
-  return <button>OFF</button>;
-}
+    const [isOff, setIsOn] = useState(true);
 
+    function handleClick() {
+        setIsOn(!isOff);
+    }
+
+    const color = isOff ? "white" : "red";
+
+    return <button style = {
+        { background: color }
+    }
+    onClick = { handleClick } > { isOff ? "ON" : "OFF" } <
+        /button>
+
+
+    // return <button onClick = { handleClick } > {
+    //     if (isOn) {
+    //         return ("ON")
+    //     } else {
+    //         "OFF"
+    //     }
+    // } < /button>;
+}
 export default Toggle;
